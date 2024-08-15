563da25f12642ddc8a673f90e6bb130109d48395
ArekFrel <arekfrel@Arek-Komputer>
2024-08-15 11:29:01 +0200

    initial commit

---
+++ nethos_landing_page/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">        
+        <img src="https://nethos.pl/assets/img/logo.png" alt="Nethos LOGO" className='logo'></img>
+      </header>
+      <body className='App-body' >Startuję z Nethos landing Page</body>
+    </div>
+  );
+}
+
+export default App;
