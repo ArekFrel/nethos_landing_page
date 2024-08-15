d1f50961353650c7eacefff31c98e2e46992a522
ArekFrel <arekfrel@Arek-Komputer>
2024-08-15 12:57:57 +0200

    second commit

--- nethos_landing_page/src/App.js
+++ nethos_landing_page/src/App.js
@@ -6,8 +6,17 @@ function App() {
     <div className="App">
       <header className="App-header">        
         <img src="https://nethos.pl/assets/img/logo.png" alt="Nethos LOGO" className='logo'></img>
+        {/* <img src="%PUBLIC_URL%/img/logo.png" alt="Nethos LOGO" className='logo'></img> */}
       </header>
-      <body className='App-body' >Startuję z Nethos landing Page</body>
+      <body className='App-body'>
+        <hero className='App-hero'>
+          <div className='App-hero-text-box'>
+          <p>Masz wiele obaw związanych z nadchodzącym wdrożeniem systemów IT w Twojej 
+            firmie? Z naszym doświadczonym zespołem konsultantów możesz mieć 
+            pewność, że Twój projekt będzie realizowany sprawnie i efektywnie!</p>
+            </div>
+        </hero>
+      </body>
     </div>
   );
 }
